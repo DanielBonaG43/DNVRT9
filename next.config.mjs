e2634530350d430f920ec448600798edfabfe1e4
DanielBonaG43 <daniel.fajarsyah@gmail.com>
2026-08-06 17:31:08 +0700

    Simplify next.config.mjs by removing Webpack config
    Removed custom Webpack configuration for externals.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Menggunakan format array external string untuk memaksa Webpack mengabaikan modul Node murni
-      config.externals = [...(config.externals || []), 'whatsapp-web.js', 'qrcode'];
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
