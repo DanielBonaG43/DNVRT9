7a10ee94cbce525add62f14b09563895462db31b
DanielBonaG43 <daniel.fajarsyah@gmail.com>
2026-08-06 17:02:24 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // KUNCI UTAMA: Beritahu Webpack untuk tidak membundel modul server biner ini
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        'whatsapp-web.js': 'commonjs whatsapp-web.js',
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
