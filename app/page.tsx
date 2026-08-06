134f638b7ec6876387bc0de1eb0b96ea9868401c
DanielBonaG43 <daniel.fajarsyah@gmail.com>
2026-08-06 11:20:41 +0700

    Add automatic redirect to login page
    Redirect users automatically from the root page to the login page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,7 @@
 
+import { redirect } from 'next/navigation';
+
+export default function RootPage() {
+  // Mengarahkan warga atau admin secara otomatis dari halaman utama (/) ke halaman (/login)
+  redirect('/login');
+}
