389d36350d3685073fc03f65f4404940dff7b4ce
DanielBonaG43 <daniel.fajarsyah@gmail.com>
2026-08-06 11:39:37 +0700

    Modify tailwind.config.js for metadata and layout
    Updated Tailwind configuration to include metadata and root layout.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+import "./globals.css"; // WAJIB DI BARIS PERTAMA
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Aplikasi RT Mandiri",
+  description: "Sistem Layanan Finansial & Administrasi Warga Digital",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="id">
+      <body className="antialiased bg-[#0F111A]">
+        {children}
+      </body>
+    </html>
+  );
 }
